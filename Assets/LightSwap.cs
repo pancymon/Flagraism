2255603a79968e3c5e8dccf1ce0736336668fb6b
ywqdl <ywqstarha@outlook.com>
2016-01-30 14:30:15 -0800

    Adding samen's magic

---
+++ Assets/LightSwap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwap : MonoBehaviour
+{
+
+    public float speed;
+    private float angle;
+    private float nextAngle;
+    // Use this for initialization
+    void Start()
+    {
+        //angle = GetComponent<Light>().spotAngle;
+        angle = 20;
+        nextAngle = Random.Range(20f, 40f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (angle > nextAngle)
+        {
+            angle -= speed*Time.deltaTime;
+        }
+        if (angle < nextAngle)
+        {
+            angle += speed * Time.deltaTime;
+        }
+        
+            nextAngle = Random.Range(20f, 40f);
+        
+        GetComponent<Light>().spotAngle = angle;
+    }
+}
