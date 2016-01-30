8a46a1fc8566d4d0ba51f2b33d76acfd16b6c9f4
ywqdl <ywqstarha@outlook.com>
2016-01-29 23:37:54 -0800

    implement scrolling background

---
+++ Assets/BackgroundScrolling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+	// Use this for initialization
+    public float speed;
+    private float offset;
+	void Start () {
+        offset = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset = offset + speed * Time.deltaTime;
+        gameObject.GetComponent<MeshRenderer>().material.mainTextureOffset = new Vector2(offset, 0);
+	
+	}
+}
