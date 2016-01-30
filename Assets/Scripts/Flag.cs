740e00a3a035b6874fe0b3a19a43f8e458a1d7f9
szhang <shujian.zhang@ndsu.edu>
2016-01-31 02:58:37 +0800

    Scripts folder, Timer script and Flag script. Add a flag and timer to scene

---
+++ Assets/Scripts/Flag.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flag : MonoBehaviour {
+
+    public float speed = 0.5f;
+    public float endPoint = 4.8f;
+    private float wholeTime;
+    private float tour;
+
+	// Use this for initialization
+	void Start () {
+        tour = endPoint - transform.position.y;
+        wholeTime = tour / speed;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+        Vector3 velocity = new Vector3(0, speed * Time.smoothDeltaTime, 0);
+
+        pos += velocity;
+
+        if (transform.position.y <= endPoint)
+            transform.position = pos;
+
+	}
+
+
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    public GameObject flagObject;
+    public Flag flag;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        flagObject = GameObject.FindWithTag("Flag");
+        if (flagObject != null)
+        {
+            flag = flagObject.GetComponent<Flag>();
+            
+        }
+        if (flag == null)
+        {
+            Debug.Log("Cannot find 'Flag' script");
+        }
+        startTime =
+            (flag.endPoint - flag.transform.position.y) / flag.speed;
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = startTime - Time.time;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f1");
+
+        if( t >= 0)
+        timerText.text = minutes + ":" + seconds;
+
+	}
+}
