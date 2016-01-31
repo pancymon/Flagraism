bea6156bee49e9b6d64112a3c2ea7ad5b4499a22
CongLiu <cliu104@ucsc.edu>
2016-01-30 18:05:43 -0800

    Start function and fail detection

---
+++ Assets/Ground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D()
+    {
+        Debug.Log("Trigger enter!");
+    }
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        //Debug.Log("collision");
+        collision2D.transform.root.SendMessage("setGameOver", 1);
+    }
+}
